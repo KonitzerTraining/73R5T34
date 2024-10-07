86f28aa47ffdb924c968c187c89f63b8acb26348
M. Konitzer <mk@konitzer-training.de>
2024-10-07 15:13:34 +0200

    spec

--- crm/src/app/features/customer/pages/customer-new/customer-new.component.spec.ts
+++ crm/src/app/features/customer/pages/customer-new/customer-new.component.spec.ts
@@ -3,17 +3,39 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CustomerNewComponent } from './customer-new.component';
 import { CustomerService } from '../../services/customer.service';
 // import { of } from 'rxjs';
-import { MockComponents } from 'ng-mocks';
+import { MockComponent, MockComponents } from 'ng-mocks';
 import { ErrorBoxComponent } from '../../../../core/components/error-box/error-box.component';
 import { LoadingIndicatorComponent } from '../../../../core/components/loading-indicator/loading-indicator.component';
 import { CustomerFormComponent } from '../../forms/customer-form/customer-form.component';
+import { Customer } from '../../model/customer';
+import { of } from 'rxjs';
+import { RouterModule } from '@angular/router';
+import { CustomerIndexComponent } from '../../customer-index.component';
 
-describe('CustomerNewComponent', () => {
+fdescribe('CustomerNewComponent', () => {
   let component: CustomerNewComponent;
   let fixture: ComponentFixture<CustomerNewComponent>;
+  let customerServiceMock: jasmine.SpyObj<CustomerService>;
 
   beforeEach(async () => {
+
+    customerServiceMock = jasmine.createSpyObj(
+      'CustomerService', 
+      [
+        'postCustomer'
+      ]);
+      
+    customerServiceMock.postCustomer.and.returnValue(of({} as Customer));
+
     await TestBed.configureTestingModule({
+      imports: [
+        RouterModule.forRoot([
+          {
+            path: 'dashboard',
+            component: MockComponent(CustomerIndexComponent)
+          }
+        ])
+      ],
       declarations: [
         CustomerNewComponent,
         MockComponents(
@@ -25,9 +47,11 @@ describe('CustomerNewComponent', () => {
       providers: [
         {
           provide: CustomerService,
-          useValue: {}
+          useValue: customerServiceMock
         }
       ],
+      // This is needed to avoid the error: 'Cannot read property 'destroy' of undefined'
+      teardown: { destroyAfterEach: false }  
     }) 
       .compileComponents();
 
@@ -38,5 +62,17 @@ describe('CustomerNewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('createCustomer', () => {
+    it('should call customerService.postCustomer', () => {
+      const customer = {
+        name: 'Test',
+        credit: 1000  
+      };
+
+     component.createCustomer(customer);
+     expect(customerServiceMock.postCustomer).toHaveBeenCalled(); 
+    });
   });
 });
--- crm/src/app/features/customer/services/customer.service.spec.ts
+++ crm/src/app/features/customer/services/customer.service.spec.ts
@@ -3,6 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { CustomerService } from './customer.service';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { Observable } from 'rxjs';
 
 describe('CustomerService', () => {
   let service: CustomerService;
@@ -23,4 +24,38 @@ describe('CustomerService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('getAll', () => {
+    it('should return an Observable<Customer[]>', () => {
+      expect(service.getAll()).toBeInstanceOf(Observable);
+    });
+  });
+
+  describe('getById', () => {
+    it('should return an Observable<Customer[]>', () => {
+      expect(service.getById(1)).toBeInstanceOf(Observable);
+    });
+  })
+
+  describe('postCustomer', () => {
+    it('should return an Observable<Customer[]>', () => {
+      expect(service.postCustomer({})).toBeInstanceOf(Observable);
+    });
+  });
+
+  describe('putCustomer', () => {
+    it('should return an Observable<Customer[]>', () => {
+      expect(service.putCustomer({
+        id: 1,
+        name: 'Test',
+        credit: 100
+      })).toBeInstanceOf(Observable);
+    });
+  });
+
+  describe('deleteById', () => {
+    it('should return an Observable<Customer[]>', () => {
+      expect(service.deleteById(1)).toBeInstanceOf(Observable);
+    });
+  })
 });
