3576bfc64413d50fe3579373b0b57b507d7c33b1
M. Konitzer <mk@konitzer-training.de>
2024-10-08 11:41:25 +0200

    Product Components

--- crm/src/app/features/product/product-index.component.ts
+++ crm/src/app/features/product/product-index.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-product-index',
   template: `
-    <p>
-      product-index works!
-    </p>
+   <h1 class="display-3">
+      Product Index
+   </h1>
+   <router-outlet></router-outlet>
   `
 })
 export class ProductIndexComponent {
--- crm/src/app/features/product/product.module.ts
+++ crm/src/app/features/product/product.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductIndexComponent } from './product-index.component';
+import { ProductListComponent } from './views/product-list/product-list.component';
+import { ProductNewComponent } from './views/product-new/product-new.component';
+import { ProductEditComponent } from './views/product-edit/product-edit.component';
 
 
 @NgModule({
   declarations: [
-    ProductIndexComponent
+    ProductIndexComponent,
+    ProductListComponent,
+    ProductNewComponent,
+    ProductEditComponent
   ],
   imports: [
     CommonModule,
