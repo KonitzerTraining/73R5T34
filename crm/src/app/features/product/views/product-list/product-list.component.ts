b8983082046d13a3a384984319441acdcff54493
M. Konitzer <mk@konitzer-training.de>
2024-10-08 15:24:27 +0200

    Delete Actions

--- crm/src/app/features/product/views/product-list/product-list.component.ts
+++ crm/src/app/features/product/views/product-list/product-list.component.ts
@@ -3,6 +3,7 @@ import { Component, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { selectError, selectLoading, selectProducts } from '../../state/selectors/product.selectors';
 import { Observable } from 'rxjs';
+import { ProductActions } from '../../state/actions/product.actions';
 
 @Component({
   selector: 'app-product-list',
@@ -11,7 +12,13 @@ import { Observable } from 'rxjs';
 })
 export class ProductListComponent {
 
-  products$ = inject(Store).select(selectProducts);
-  loading$: Observable<null | boolean> = inject(Store).select(selectLoading);
-  errorMessage$ = inject(Store).select(selectError);
+  readonly #store = inject(Store);
+  products$ = this.#store.select(selectProducts);
+  loading$: Observable<null | boolean> = this.#store.select(selectLoading);
+  errorMessage$ =   this.#store.select(selectError);
+
+  deleteProduct(productId: number) {
+    // Delete product
+    this.#store.dispatch(ProductActions.deleteProduct({ productId }));
+  }
 }
