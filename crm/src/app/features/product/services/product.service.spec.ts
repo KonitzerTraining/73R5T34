9f1730f1191cd7909ac0959780dc8eca5ec0c0b3
M. Konitzer <mk@konitzer-training.de>
2024-10-08 12:26:32 +0200

    Unit Test für product.service.ts

--- crm/src/app/features/product/services/product.service.spec.ts
+++ crm/src/app/features/product/services/product.service.spec.ts
@@ -1,16 +1,30 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProductService } from './product.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { Observable } from 'rxjs';
 
 describe('ProductService', () => {
   let service: ProductService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    });
     service = TestBed.inject(ProductService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getAll', () => {
+    it('should return an Observable<Customer[]>', () => {
+      expect(service.getProducts()).toBeInstanceOf(Observable);
+    });
   });
 });
--- crm/src/app/features/product/services/product.service.ts
+++ crm/src/app/features/product/services/product.service.ts
@@ -11,7 +11,11 @@ const url = environment.api + 'products/';
 })
 export class ProductService {
 
-  #http = inject(HttpClient);
+  /**
+   *  MDN private class fields 
+   * (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/Private_class_fields)
+   */
+  readonly #http = inject(HttpClient);
   
   getProducts(): Observable<Product[]> {
     return this.#http.get<Product[]>(url);
