f847af43f9591178e67a0fe1dc63d644378770d5
M. Konitzer <mk@konitzer-training.de>
2024-10-08 10:23:47 +0200

    CustomerForm Spec

--- crm/src/app/features/customer/forms/customer-form/customer-form.component.spec.ts
+++ crm/src/app/features/customer/forms/customer-form/customer-form.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CustomerFormComponent } from './customer-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { customersMock } from '../../../../../../__mocks__/api/customers';
 
 describe('CustomerFormComponent', () => {
   let component: CustomerFormComponent;
@@ -18,10 +19,30 @@ describe('CustomerFormComponent', () => {
 
     fixture = TestBed.createComponent(CustomerFormComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('formSubmit', () => {
+    it(('should emit form value'), () => {
+      const emitSpy = spyOn(component.customerSubmit, 'emit');
+      component.formSubmit();
+      expect(emitSpy).toHaveBeenCalledWith(component.customerFormGroup.value);
+    });
+  });
+
+  
+  describe('CustomerForm for Edit Customers', () => {
+    it(('should patchValue'), () => {
+      const patchValueSpy = spyOn(component.customerFormGroup, 'patchValue');
+      
+      component.data = customersMock[0];
+      component.ngOnInit();
+
+      expect(patchValueSpy).toHaveBeenCalledWith(customersMock[0]);
+    });
   });
 });
