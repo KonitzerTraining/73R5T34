7bbd44b1b21c2f69a222cee0c08aa223a77567ad
M. Konitzer <mk@konitzer-training.de>
2024-10-08 14:41:11 +0200

    LoadingBock und error

--- crm/src/app/features/product/product.module.ts
+++ crm/src/app/features/product/product.module.ts
@@ -11,6 +11,7 @@ import { EffectsModule } from '@ngrx/effects';
 import { ProductEffects } from './state/effects/product.effects';
 import { StoreModule } from '@ngrx/store';
 import { productFeature, productFeatureKey } from './state/reducers/product.reducer';
+import { CoreModule } from '../../core/core.module';
 
 
 @NgModule({
@@ -22,6 +23,8 @@ import { productFeature, productFeatureKey } from './state/reducers/product.redu
   ],
   imports: [
     CommonModule,
+    CoreModule,
+    
     ProductRoutingModule,
     StoreModule.forFeature(productFeatureKey, productFeature.reducer),
     EffectsModule.forFeature([ProductEffects])
--- crm/src/app/features/product/views/product-list/product-list.component.ts
+++ crm/src/app/features/product/views/product-list/product-list.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 
 import { Store } from '@ngrx/store';
 import { selectError, selectLoading, selectProducts } from '../../state/selectors/product.selectors';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-list',
@@ -11,6 +12,6 @@ import { selectError, selectLoading, selectProducts } from '../../state/selector
 export class ProductListComponent {
 
   products$ = inject(Store).select(selectProducts);
-  loading$ = inject(Store).select(selectLoading);
+  loading$: Observable<null | boolean> = inject(Store).select(selectLoading);
   errorMessage$ = inject(Store).select(selectError);
 }
