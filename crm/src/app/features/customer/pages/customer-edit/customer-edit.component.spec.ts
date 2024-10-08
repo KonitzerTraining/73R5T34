703e8febbbd6dbbe0c8c61c0055ec08be27bd1a1
M. Konitzer <mk@konitzer-training.de>
2024-10-08 09:57:27 +0200

    Spec LoadCustomer

--- crm/src/app/features/customer/pages/customer-edit/customer-edit.component.spec.ts
+++ crm/src/app/features/customer/pages/customer-edit/customer-edit.component.spec.ts
@@ -3,15 +3,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CustomerEditComponent } from './customer-edit.component';
 import { RouterModule } from '@angular/router';
 import { CustomerService } from '../../services/customer.service';
-import { MockComponents } from 'ng-mocks';
+import { MockComponent, MockComponents } from 'ng-mocks';
 import { ErrorBoxComponent } from '../../../../core/components/error-box/error-box.component';
 import { LoadingIndicatorComponent } from '../../../../core/components/loading-indicator/loading-indicator.component';
 import { CustomerFormComponent } from '../../forms/customer-form/customer-form.component';
 import { of, throwError } from 'rxjs';
 import { customersMock } from '../../../../../../__mocks__/api/customers';
 import { createCustomerServiceMock } from '../../../../../../__mocks__/services/customer.service.mock';
+import { CustomerIndexComponent } from '../../customer-index.component';
 
-fdescribe('CustomerEditComponent', () => {
+describe('CustomerEditComponent', () => {
   let component: CustomerEditComponent;
   let fixture: ComponentFixture<CustomerEditComponent>;
   let customerServiceMock: jasmine.SpyObj<CustomerService>;
@@ -21,7 +22,12 @@ fdescribe('CustomerEditComponent', () => {
 
     await TestBed.configureTestingModule({
       imports: [
-        RouterModule.forRoot([]),
+        RouterModule.forRoot([
+          {
+            path: 'dashboard',
+            component: MockComponent(CustomerIndexComponent)
+          }
+        ])
       ],
       declarations: [
         CustomerEditComponent,
@@ -30,30 +36,56 @@ fdescribe('CustomerEditComponent', () => {
           LoadingIndicatorComponent,
           CustomerFormComponent
         )],
-        providers: [
-          {
-            provide: CustomerService,
-            useValue: customerServiceMock
-          }
-        ],
+      providers: [
+        {
+          provide: CustomerService,
+          useValue: customerServiceMock
+        }
+      ],
+      // This is needed to avoid the error: 'Cannot read property 'destroy' of undefined'
+      teardown: { destroyAfterEach: false }
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(CustomerEditComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
+  describe('loadCustomer', () => {
+    it('should call customerService.getById', () => {
+      component.id = customersMock[0].id;
+
+      fixture.detectChanges(); // triggers ngOnInit -> loadCustomers -> customerService.getAll
+      expect(customerServiceMock.getById).toHaveBeenCalledOnceWith(customersMock[0].id);
+    });
+
+    it('should handle error', () => {
+      component.id = customersMock[0].id;
+      const errorMessage = 'Error message';
+      customerServiceMock.getById.and.callFake(() => {
+        return throwError(() => {
+          return new Error(errorMessage);
+        })
+      });
+
+      // fixture.detectChanges(); // triggers ngOnInit -> loadCustomers -> customerService.getAll
+      component.loadCustomer();
+      expect(component.errorMessage).toBe(errorMessage);
+    });
+  });
+
+
   describe('updateCustomer', () => {
     it('should call customerService.putCustomer', () => {
       const customer = customersMock[0]
-    
-     component.updateCustomer(customer);
-     expect(customerServiceMock.putCustomer).toHaveBeenCalled(); 
+
+      component.updateCustomer(customer);
+      expect(customerServiceMock.putCustomer).toHaveBeenCalled();
     });
 
     it('should handle error', () => {
