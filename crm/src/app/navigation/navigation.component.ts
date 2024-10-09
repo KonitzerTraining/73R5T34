c28be8671ee08d5271e3348025ada2040f1cf0db
M. Konitzer <mk@konitzer-training.de>
2024-10-09 11:09:26 +0200

    Navigation

--- crm/src/app/navigation/navigation.component.ts
+++ crm/src/app/navigation/navigation.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AuthActions } from '../features/auth/state/actions/auth.actions';
+import { selectIsAuthenticated, selectUserName } from '../features/auth/state/selectors/auth.selectors';
 
 @Component({
   selector: 'app-navigation',
@@ -9,7 +10,11 @@ import { AuthActions } from '../features/auth/state/actions/auth.actions';
 })
 export class NavigationComponent {
 
+  
   #store = inject(Store);
+  isAuthenticated$ = this.#store.select(selectIsAuthenticated);
+  userName$ = this.#store.select(selectUserName);
+
 
   logout() {
     this.#store.dispatch(AuthActions.logout());
