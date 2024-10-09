bc9bf46c0382ec99256879e5723fa70e5334e43f
M. Konitzer <mk@konitzer-training.de>
2024-10-09 09:46:57 +0200

    effect

--- crm/src/app/features/auth/state/effects/auth.effects.ts
+++ crm/src/app/features/auth/state/effects/auth.effects.ts
@@ -1,13 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { catchError, map, concatMap } from 'rxjs/operators';
 import { Observable, EMPTY, of } from 'rxjs';
 import { AuthActions } from '../actions/auth.actions';
+import { AuthService } from '../../services/auth.service';
 
 
 @Injectable()
 export class AuthEffects {
 
+  #actions$ = inject(Actions);
+  #auth = inject(AuthService);
 
-  constructor(private actions$: Actions) {}
+  login$ = createEffect(() => this.#actions$.pipe(
+    ofType(AuthActions.login),
+    concatMap(({ login }) => this.#auth.getUser(login).pipe(
+      map(user => AuthActions.loginSuccess({ user })),
+      catchError(error => of(AuthActions.loginFailure({ error })))
+    ))
+  ));
+
+
+
 }
